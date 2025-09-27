de85f1889c47edb0290f45350a60c404ac337569
exsarorrayzer <theleapy@gmail.com>
2025-09-27 20:50:55 +0400

    Update creds.py

--- modules/creds.py
+++ modules/creds.py
@@ -9,22 +9,22 @@ Instagram: exsarorrayzer
 Discord:   noinfonocontext
 Youtube:   exsarorrayzer"""  # By Rayzer
 
-    # Mor tonları (RGB)  # By Rayzer
-    purple_shades = [
-        (102, 0, 153),
-        (128, 0, 192),
-        (153, 51, 255),
-        (178, 102, 255),
-        (153, 51, 255),
-        (128, 0, 192),
-        (102, 0, 153)
+    # Kirmizi tonları (RGB)  # By Rayzer
+    red_shades = [
+        (255, 0, 0),
+        (255, 0, 25),
+        (255, 0, 30),
+        (255, 0, 45),
+        (255, 0, 55),
+        (255, 0, 75),
+        (255, 0, 80)
     ]  # By Rayzer
 
     lines = info.strip("\n").split("\n")  # By Rayzer
     colored_lines = []  # By Rayzer
 
     for i, line in enumerate(lines):  # By Rayzer
-        color = purple_shades[i % len(purple_shades)]  # By Rayzer
+        color = red_shades[i % len(purple_shades)]  # By Rayzer
         hex_color = f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}"  # By Rayzer
         colored_lines.append(f"[{hex_color}]{line}[/{hex_color}]")  # By Rayzer
 
